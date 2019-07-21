cf7529ebdab24d9dd8bd13023c62dad92caab486
Mondal10 <amitmondal111.am@gmail.com>
2019-07-21 21:49:28 +0530

    Copy to clipboard fully functioning now

--- src/javascript/modules/copy.js
+++ src/javascript/modules/copy.js
@@ -1,12 +1,28 @@
+
 export const copy = () => {
 
     const copyBtn = document.querySelector('.copy-btn');
 
     copyBtn.addEventListener('click', copyText);
-
 }
 
-const copyText = () => {
+/**
+ * Copy to clipboard function
+ */
+const copyText = (e) => {
+    // const { toElement: element, } = e; // Get clicked element
     const content = document.querySelector('.words');
-    console.log(content.innerText);
+
+    /**
+     * This gets a selection object, then creates a range where we select 
+     * the contents our own div element which is editable.
+     */
+    const selection = window.getSelection();
+    const range = document.createRange();
+
+    range.selectNodeContents(content);
+    selection.removeAllRanges(); // make sure that the global selection doesn’t contain any range yet
+    selection.addRange(range); // add our new range to the selection
+
+    document.execCommand('copy');
 }
